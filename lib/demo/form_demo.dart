37a6e4ecaa725db592c4c2b1b88d758ee718dfa8
mayansong@zhilunkeji.com <mayansong@zhilunkeji.com>
2018-12-03 11:11:12 +0800

    fourth commit

--- lib/demo/form_demo.dart
+++ lib/demo/form_demo.dart
@@ -12,12 +12,97 @@ class ForeDemo extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                TextFiledDemo()
-
+                LoginForm(),
               ],
             ),
           )),
     );
+  }
+}
+
+class LoginForm extends StatefulWidget {
+  LoginFormState createState() => LoginFormState();
+}
+
+class LoginFormState extends State<LoginForm> {
+  final loginFormKey = GlobalKey<FormState>();
+  String username, password;
+  bool validateState = false;
+
+  void onLoginFormSubmit() {
+    if (loginFormKey.currentState.validate()) {
+      loginFormKey.currentState.save();
+      debugPrint('Username: $username');
+      debugPrint('Password: $password');
+      Scaffold.of(context).showSnackBar(SnackBar(
+        content: Text('Logining...',),
+      ));
+    } else {
+      setState(() {
+        validateState = true;
+      });
+    }
+  }
+
+  String validateUsername(value) {
+    if (value.isEmpty) {
+      return 'Username is required';
+    }
+    return null;
+  }
+
+  String validatePassword(value) {
+    if (value.isEmpty) {
+      return 'Password is required';
+    }
+    return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Form(
+        key: loginFormKey,
+        child: Column(
+          children: <Widget>[
+            TextFormField(
+              decoration:
+                  InputDecoration(labelText: 'Username', helperText: ''),
+              validator: validateUsername,
+              autovalidate: validateState,
+              onSaved: (value) {
+                username = value;
+              },
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration:
+                  InputDecoration(labelText: 'Password', helperText: ''),
+              validator: validatePassword,
+              autovalidate: validateState,
+              onSaved: (value) {
+                password = value;
+              },
+            ),
+            SizedBox(
+              height: 40.0,
+            ),
+            Container(
+              width: double.infinity,
+              child: RaisedButton(
+                  color: Colors.blue,
+                  child: Text(
+                    'Login',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  elevation: 0.0,
+                  onPressed: () {
+                    onLoginFormSubmit();
+                  }),
+              color: Colors.blue,
+            ),
+          ],
+        ));
   }
 }
 
@@ -30,16 +115,37 @@ class TextFiledDemo extends StatefulWidget {
 }
 
 class TextFeildState extends State<TextFiledDemo> {
+  final textFeildController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    textFeildController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    textFeildController.addListener(() {
+      debugPrint('input text: ${textFeildController.text}');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return TextField(
+      controller: textFeildController,
+      onSubmitted: (value) {
+        debugPrint('submit : $value');
+      },
       decoration: InputDecoration(
         icon: Icon(Icons.subject),
         labelText: 'name',
         hintText: 'Please Input Your Name',
         border: OutlineInputBorder(),
-
         filled: true,
       ),
     );
