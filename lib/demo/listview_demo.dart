691daf82f2b9703eed49613c35d717233d234e16
mayansong@zhilunkeji.com <mayansong@zhilunkeji.com>
2018-12-12 11:35:00 +0800

    add listview_demo

---
+++ lib/demo/listview_demo.dart
@@ -0,0 +1,42 @@
+import 'package:myflutter/model/post.dart';
+import 'package:flutter/material.dart';
+
+class ListViewDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(itemBuilder: _itemBuilder);
+  }
+
+  Widget _itemBuilder(BuildContext context, int index) {
+    return Container(
+      color: Colors.white,
+      padding: EdgeInsets.all(6.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          AspectRatio(
+            aspectRatio: 16 / 9,
+            child: Image.network(
+              posts[index].imageUrl,
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(
+            height: 16.0,
+          ),
+          Text(
+            posts[index].title,
+            style: Theme.of(context).textTheme.title,
+          ),
+          Text(
+            posts[index].author,
+            style: Theme.of(context).textTheme.subhead,
+          ),
+          SizedBox(
+            height: 26.0,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:myflutter/model/post.dart';
+import 'package:myflutter/demo/listview_demo.dart';
 
 class Home extends StatelessWidget {
   @override
@@ -13,40 +13,9 @@ class Home extends StatelessWidget {
         elevation: 0.0,
         centerTitle: true, // 标题是否位于标题栏中间
       ),
-      body: ListView.builder(itemBuilder: _itemBuilder),
+      body: ListViewDemo(),
     );
   }
 
-  Widget _itemBuilder(BuildContext context, int index) {
-    return Container(
-      color: Colors.white,
-      padding: EdgeInsets.all(6.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          AspectRatio(
-            aspectRatio: 16 / 9,
-            child: Image.network(
-              posts[index].imageUrl,
-              fit: BoxFit.fill,
-            ),
-          ),
-          SizedBox(
-            height: 16.0,
-          ),
-          Text(
-            posts[index].title,
-            style: Theme.of(context).textTheme.title,
-          ),
-          Text(
-            posts[index].author,
-            style: Theme.of(context).textTheme.subhead,
-          ),
-          SizedBox(
-            height: 26.0,
-          )
-        ],
-      ),
-    );
-  }
+
 }
